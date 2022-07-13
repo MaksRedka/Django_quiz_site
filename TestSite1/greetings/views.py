18c5bfb4e04671757f94e950b5f4cdd11e76b566
MaksRedka <mex4232@gmail.com>
2022-07-14 00:00:30 +0300

    Project with one app

---
+++ TestSite1/greetings/views.py
@@ -0,0 +1,11 @@
+import re
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello world")
+
+def test(request):
+    return HttpResponse("<h1>I'm on test page</h1>")
